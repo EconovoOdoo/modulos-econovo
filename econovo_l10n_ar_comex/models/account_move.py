bfd292f3a4cefd201a8d14732a6757d6050275a6
Jose D. Leonett <josed.95@gmail.com>
2026-02-05 12:27:54 -0300

    [ADD] econovo_l10n_ar_comex: COMEX fields in account.move
    Added inverse relationship fields from invoices to COMEX:
    
    1. New model account_move.py:
       - comex_operation_ids: M2M to COMEX operations
       - comex_clearance_ids: O2M from customs clearances
       - comex_clearance_count: computed field
       - action_view_comex_operations(): open linked operations
       - action_view_comex_clearances(): open linked clearances
    
    2. New view account_move_views.xml:
       - COMEX page in notebook (visible for vendor bills)
       - Fields to link invoice to operations and clearances
       - Smart buttons showing COMEX ops and clearances count
       - Information alerts for user guidance
    
    3. Benefits:
       - Bidirectional navigation: invoice  COMEX operation
       - See which clearances use a tribute invoice
       - Link invoices to operations from Accounting app
       - Full traceability from both perspectives
    
    User can now:
    - From invoice (action=347&model=account.move): see linked operations/clearances
    - From operation: see linked invoices (already existed)
    - From clearance: see vendor bill (already existed)

---
+++ econovo_l10n_ar_comex/models/account_move.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# Part of Econovo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class AccountMove(models.Model):
+    """Extend account.move to add COMEX operation and clearance links."""
+    
+    _inherit = 'account.move'
+
+    # -------------------------------------------------------------------------
+    # FIELDS
+    # -------------------------------------------------------------------------
+    comex_operation_ids = fields.Many2many(
+        'comex.operation',
+        string="COMEX Operations",
+        help="COMEX operations linked to this invoice/bill",
+        copy=False,
+    )
+    comex_clearance_ids = fields.One2many(
+        'comex.customs.clearance',
+        'vendor_bill_id',
+        string="Customs Clearances",
+        help="Customs clearances that use this vendor bill for tributes",
+        copy=False,
+    )
+    comex_clearance_count = fields.Integer(
+        string="Clearances Count",
+        compute='_compute_comex_clearance_count',
+        store=False,
+    )
+
+    # -------------------------------------------------------------------------
+    # COMPUTE METHODS
+    # -------------------------------------------------------------------------
+    def _compute_comex_clearance_count(self):
+        """Count linked customs clearances."""
+        for move in self:
+            move.comex_clearance_count = len(move.comex_clearance_ids)
+
+    # -------------------------------------------------------------------------
+    # ACTION METHODS
+    # -------------------------------------------------------------------------
+    def action_view_comex_operations(self):
+        """Open COMEX operations linked to this invoice."""
+        self.ensure_one()
+        action = self.env.ref('econovo_l10n_ar_comex.action_comex_operation').read()[0]
+        if len(self.comex_operation_ids) > 1:
+            action['domain'] = [('id', 'in', self.comex_operation_ids.ids)]
+        elif len(self.comex_operation_ids) == 1:
+            action['views'] = [(self.env.ref('econovo_l10n_ar_comex.view_comex_operation_form').id, 'form')]
+            action['res_id'] = self.comex_operation_ids.id
+        else:
+            action = {'type': 'ir.actions.act_window_close'}
+        return action
+
+    def action_view_comex_clearances(self):
+        """Open customs clearances linked to this invoice."""
+        self.ensure_one()
+        action = self.env.ref('econovo_l10n_ar_comex.action_comex_customs_clearance').read()[0]
+        if len(self.comex_clearance_ids) > 1:
+            action['domain'] = [('id', 'in', self.comex_clearance_ids.ids)]
+        elif len(self.comex_clearance_ids) == 1:
+            action['views'] = [(self.env.ref('econovo_l10n_ar_comex.view_comex_customs_clearance_form').id, 'form')]
+            action['res_id'] = self.comex_clearance_ids.id
+        else:
+            action = {'type': 'ir.actions.act_window_close'}
+        return action
