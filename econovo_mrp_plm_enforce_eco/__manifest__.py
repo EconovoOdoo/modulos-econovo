1e54a80e54b571bebf5c87e2d049e2bf34b772c6
Jose D. Leonett <josed.95@gmail.com>
2026-06-25 15:47:16 -0300

    [IMP] econovo_mrp_plm_enforce_eco: bump version to 17.0.1.1.0

--- econovo_mrp_plm_enforce_eco/__manifest__.py
+++ econovo_mrp_plm_enforce_eco/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'PLM Enforce ECO Workflow',
-    'version': '17.0.1.0.0',
+    'version': '17.0.1.1.0',
     'category': 'Manufacturing/Manufacturing',
     'summary': 'Force BoM modifications through the PLM Engineering Change Order workflow for controlled editors.',
     'description': """
